2c64a76714a2642a74735a3df59b2a4565015eec
Nissim Buchs <nissim@buchs.be>
2025-12-27 10:54:49 +0100

    chore: add missing @Disabled import for UserPreferencesControllerIntegrationTest

--- services/company-user-management-service/src/test/java/ch/batbern/companyuser/controller/UserPreferencesControllerIntegrationTest.java
+++ services/company-user-management-service/src/test/java/ch/batbern/companyuser/controller/UserPreferencesControllerIntegrationTest.java
@@ -9,6 +9,7 @@ import ch.batbern.shared.test.AbstractIntegrationTest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Disabled;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
